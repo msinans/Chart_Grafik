bb7d6e194e54f521c3ec684e578c6537ba5e2b95
m.sinans <msomercikoglu@gmail.com>
2023-03-15 13:06:22 +0300

    15.03.2023

---
+++ Chart_Grafik/Form3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chart_Grafik
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+         /* chart1.Series["Otomobil"].Points.AddXY("BMW", 250);
+            chart1.Series["Otomobil"].Points.AddXY("Mercedes", 240);
+            chart1.Series["Otomobil"].Points.AddXY("Ami", 40);
+            chart1.Series["Otomobil"].Points.AddXY("Porsche", 300);
+            chart1.Series["Otomobil"].Points.AddXY("Bugatti", 350);
+         */
+
+            string[] otomobil = { "BMW", "Mercedes", "Ami", "Porsche", "Şahin", "Bugatti" };
+            int[] hiz = { 250, 240, 40, 300, 180, 350 };
+
+            for (int i = 0; i < otomobil.Length; i++)
+            {
+                chart1.Series["Otomobil"].Points.AddXY(otomobil[i], hiz[i]);
+            }
+
+        }
+    }
+}
--- Chart_Grafik/Program.cs
+++ Chart_Grafik/Program.cs
@@ -16,7 +16,7 @@ namespace Chart_Grafik
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form2());
+            Application.Run(new Form3());
         }
     }
 }
